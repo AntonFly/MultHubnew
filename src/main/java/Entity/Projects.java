36371062d168e251d46f451e6d5fd71e2753e3d1
Anton_fly <Viavaeltorro>
2018-12-21 17:57:36 +0300

    Ola

--- src/main/java/Entity/Projects.java
+++ src/main/java/Entity/Projects.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="projects")
-@NamedQuery(name = "Users.getAll", query = "SELECT u from Users u")
+@NamedQuery(name = "Projects.getAll", query = "SELECT u from Projects u")
 public class Projects {
     public Projects(){}
 
@@ -19,7 +19,55 @@ public class Projects {
     private Double curbudget;
     @Column
     private Double goalbudget;
-    @OneToOne
+    @OneToOne(optional = false,cascade = CascadeType.ALL)
+    @JoinColumn(name ="projectid")
     private Creditinfo credit;
 
+    public String getProjectid() {
+        return projectid;
+    }
+
+    public void setProjectid(String projectid) {
+        this.projectid = projectid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getCurbudget() {
+        return curbudget;
+    }
+
+    public void setCurbudget(Double curbudget) {
+        this.curbudget = curbudget;
+    }
+
+    public Double getGoalbudget() {
+        return goalbudget;
+    }
+
+    public void setGoalbudget(Double goalbudget) {
+        this.goalbudget = goalbudget;
+    }
+
+    public Creditinfo getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Creditinfo credit) {
+        this.credit = credit;
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+import Entity.Projects;
 import Entity.Users;
 
 import javax.persistence.EntityManager;
@@ -5,18 +6,34 @@ import javax.persistence.Persistence;
 import java.util.List;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         EntityManager em = Persistence.createEntityManagerFactory("MULTHUB").createEntityManager();
         em.getTransaction().begin();
         List<Users> users = em.createNamedQuery("Users.getAll").getResultList();
-        for (Users user:
-             users) {
-            if(user.getLogin().equals("aad")){
+        em.getTransaction().commit();
+        for (Users user: users)
+        {
+            if(user.getLogin().equals("aad"))
+            {
                 System.out.println( user.getCondata().geteMail());
             }
         }
 
 
-    }
+        //EntityManager em2 = Persistence.createEntityManagerFactory("MULTHUB").createEntityManager();
+        em.getTransaction().begin();
+        List<Projects> projects = em.createNamedQuery("Projects.getAll").getResultList();
+        //em.getTransaction().commit();
+        for (Projects project: projects)
+        {
+            if(project.getProjectid().equals("123456789"))
+            {
+                System.out.println( project.getProjectid()+" WORKS!!");
+            }
+        }
 
+
+
+    }
 }
